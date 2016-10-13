8bed38578822d5600aca169df540286939f4103a
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-13 13:55:32 -0700

    renames question model file so it communicates with db....adds working seed file....adds password methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  # has_secure_password
   # Remember to create a migration!
   has_many :questions
   has_many :answers
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
 
   include BCrypt
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(pass)
+    @password = Password.create(pass)
+    self.hashed_password = @password
+  end
 end
