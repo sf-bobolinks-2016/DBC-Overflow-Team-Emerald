c64cd1f7ce7617703c43ef7b76bcbe53f634a233
marvan_t <marvan_t@hotmail.com>
2016-10-13 12:30:07 -0700

    user migration, model, controller, and sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
   # Remember to create a migration!
+  has_many :questions
+  has_many :answers
+
+  validates :email, :hashed_password, presence: true
+  validates :email, uniqueness: true
+
+  include BCrypt
 end
