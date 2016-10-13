2a9e5578a5c55592f5f185e0cf993fbb9cbc1647
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-13 15:07:12 -0700

    adds working user reg and sessions helpers

---
+++ app/controllers/users.rb
@@ -0,0 +1,15 @@
+#registration
+get '/users/new' do
+	erb :'users/new'
+end
+
+post '/users' do
+	if params[:password] == params[:password_confirm]
+		user = User.new(email: 	 params[:email],
+										password: params[:password])
+		if user.save
+			session[:id] = user.id
+		end
+	end
+	redirect "/"
+end
