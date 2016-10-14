9f561b77feaa5ac72831958c879ed342ea9f8f7a
Sam Leiken <sam.k.leiken@gmail.com>
2016-10-14 11:45:18 -0700

    reformats answers post route

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,6 +1,6 @@
 post '/answers' do
-  p params
-  answer = Answer.create(answer: params[:answer], user_id: session[:user_id],question_id: params[:question_id]
-    )
+  Answer.create(answer: params[:answer],
+                user_id: session[:user_id],
+                question_id: params[:question_id])
   redirect "/questions/#{params[:question_id]}"
 end
